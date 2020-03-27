63d37a482d463c22dc973199193ef4dba1c9b2b8
dalecars <carson.dale@gmail.com>
2020-03-27 16:44:31 +0000

    coffee belt added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 function toggleSidePanel() {
     if ($(".sidePanel").width() === 0) {
-        $(".sidePanel").width("40%");
+        $(".sidePanel").width("60%");
+        $('body').addClass('noScroll');
     } else {
         $(".sidePanel").width(0);
+        $('body').removeClass('noScroll');
     }
 }
+
+function changeIcon(icon) {
+  icon.classList.toggle("change");
+}
+
+$(document).ready(function(){
+    $('.scalize').scalize({
+        width : 800,
+        height : 500,
+        animationSelector :'pulse',
+    });
+});
